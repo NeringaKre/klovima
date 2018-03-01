83247e18dd0022dc96d130402e1424ed0afa3d6f
NeringaKre <n.kretkovskyte@gmail.com>
2018-03-01 18:04:23 +0200

    wrapping up the project

--- dist/bundle.js
+++ dist/bundle.js
@@ -1239,6 +1239,7 @@
 	        navigationPosition: 'right',
 	        navigationTooltips: ['','Traktoriai', 'Kombainai', 'Krautuvai', 'Savivarčių priekabos', 'Puspriekabės', "Vilkikai"],
 	        showActiveTooltip: true,
+	        scrollingSpeed: 700,
 	      });
 	    }
 
@@ -1271,14 +1272,29 @@
 	    });
 
 	    $('.language-menu').click(function() {
-	      $('.language-menu li').toggle("");
+	      $(".language-menu > div").toggleClass("active");
 	    });
+
+	      //  $(document.body).click(function() {
+	      //     $(".language-menu > div").removeClass("active");     
+	      // });
+
+
+	    $('.language-menu > div').on('focusout', function () {
+	      $(this).removeClass('active');
+	    });
+
 
 	    $('.paging ul li').click(function() {
 	      $('.paging ul li').removeClass("active");
 	      $(this).toggleClass("active");
 	    });
 
+
+	    $('.nuoma-vidinis .isorinis__heading a p').click(function() {
+	      $('.nuoma-vidinis .isorinis__heading a p').removeClass("active");
+	      $(this).addClass("active");
+	    });
 
 	    $('.mokymai .isorinis__heading a p').click(function() {
 	      $('.mokymai .isorinis__heading a p').removeClass("active");
@@ -1291,6 +1307,8 @@
 
 	    $('.isorinis__info-blocks--second-line').matchHeight();
 
+	    $('.similar').matchHeight();
+
 	    $('.header__menu li').click(function() {
 	      $('.header__menu li').removeClass("active2");
 	      $(this).addClass("active2");
@@ -1301,8 +1319,64 @@
 	        $('.header__menu').removeClass('toggle-menu');
 	      }
 	    });
+
+	    // $(".main-section__info-block .btn__red--big").click(function() {
+	    //   $('body').animate({
+	    //     scrollTop: $(".main-section__second").offset().top},'slow');
+	    // });
+
+	    //scroll arrows
+
+	    $( ".other-pages__arrow" ).click(function(e) {
+	      e.preventDefault(), $("html, body").animate({
+	        scrollTop: $(".other-pages__arrow").offset().top + 30
+	      }, 1e3)
+	    });
+
+	    $( ".other-pages__arrow2" ).click(function(f) {
+	      f.preventDefault(), $("html, body").animate({
+	        scrollTop: $(".other-pages__arrow2").offset().top + 30
+	      }, 1e3)
+	    });
+
+	    $(".main-section__arrow").click(function(f) {
+	      $.fn.fullpage.moveSectionDown();
+	    });
+
+	    $(".main-section__info-block .btn__red--big").click(function(f) {
+	      $.fn.fullpage.moveSectionDown();
+	    });
+
+	    if ($('#lightSlider').length !== 0) {
+	      if ($(window).width() > 1024) {
+	        $('#lightSlider').lightSlider({
+	          gallery: true,
+	          item: 1,
+	          loop: true,
+	          sliderMargin: 0,
+	          thumbItem: 4,
+	        });
+	      }
+	      
+	      if ($(window).width() <= 1024) {
+	        $('#lightSlider').lightSlider({
+	          gallery: true,
+	          item: 1,
+	          loop: true,
+	          sliderMargin: 0,
+	          thumbItem: 8,
+	        });
+	      }
+	    }
 	  });
-	  
+
+	  $(document).mouseup(function(e) {
+	    var languageContainer = $('.language-menu > div');
+
+	    if (!languageContainer.is(e.target) && languageContainer.has(e.target).length === 0) {
+	      languageContainer.removeClass('active');
+	    }
+	  });
 	})(jQuery);
 
 
--- js/site.js
+++ js/site.js
@@ -6,6 +6,7 @@
         navigationPosition: 'right',
         navigationTooltips: ['','Traktoriai', 'Kombainai', 'Krautuvai', 'Savivarčių priekabos', 'Puspriekabės', "Vilkikai"],
         showActiveTooltip: true,
+        scrollingSpeed: 700,
       });
     }
 
@@ -38,14 +39,29 @@
     });
 
     $('.language-menu').click(function() {
-      $('.language-menu li').toggle("");
+      $(".language-menu > div").toggleClass("active");
     });
+
+      //  $(document.body).click(function() {
+      //     $(".language-menu > div").removeClass("active");     
+      // });
+
+
+    $('.language-menu > div').on('focusout', function () {
+      $(this).removeClass('active');
+    });
+
 
     $('.paging ul li').click(function() {
       $('.paging ul li').removeClass("active");
       $(this).toggleClass("active");
     });
 
+
+    $('.nuoma-vidinis .isorinis__heading a p').click(function() {
+      $('.nuoma-vidinis .isorinis__heading a p').removeClass("active");
+      $(this).addClass("active");
+    });
 
     $('.mokymai .isorinis__heading a p').click(function() {
       $('.mokymai .isorinis__heading a p').removeClass("active");
@@ -58,6 +74,8 @@
 
     $('.isorinis__info-blocks--second-line').matchHeight();
 
+    $('.similar').matchHeight();
+
     $('.header__menu li').click(function() {
       $('.header__menu li').removeClass("active2");
       $(this).addClass("active2");
@@ -68,6 +86,62 @@
         $('.header__menu').removeClass('toggle-menu');
       }
     });
+
+    // $(".main-section__info-block .btn__red--big").click(function() {
+    //   $('body').animate({
+    //     scrollTop: $(".main-section__second").offset().top},'slow');
+    // });
+
+    //scroll arrows
+
+    $( ".other-pages__arrow" ).click(function(e) {
+      e.preventDefault(), $("html, body").animate({
+        scrollTop: $(".other-pages__arrow").offset().top + 30
+      }, 1e3)
+    });
+
+    $( ".other-pages__arrow2" ).click(function(f) {
+      f.preventDefault(), $("html, body").animate({
+        scrollTop: $(".other-pages__arrow2").offset().top + 30
+      }, 1e3)
+    });
+
+    $(".main-section__arrow").click(function(f) {
+      $.fn.fullpage.moveSectionDown();
+    });
+
+    $(".main-section__info-block .btn__red--big").click(function(f) {
+      $.fn.fullpage.moveSectionDown();
+    });
+
+    if ($('#lightSlider').length !== 0) {
+      if ($(window).width() > 1024) {
+        $('#lightSlider').lightSlider({
+          gallery: true,
+          item: 1,
+          loop: true,
+          sliderMargin: 0,
+          thumbItem: 4,
+        });
+      }
+      
+      if ($(window).width() <= 1024) {
+        $('#lightSlider').lightSlider({
+          gallery: true,
+          item: 1,
+          loop: true,
+          sliderMargin: 0,
+          thumbItem: 8,
+        });
+      }
+    }
   });
-  
+
+  $(document).mouseup(function(e) {
+    var languageContainer = $('.language-menu > div');
+
+    if (!languageContainer.is(e.target) && languageContainer.has(e.target).length === 0) {
+      languageContainer.removeClass('active');
+    }
+  });
 })(jQuery);
